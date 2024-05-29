0c122c0f996b2c0b00ba25a6e6556277a0512d77
Andy Rabearimanana <andyrabearimanana@gmail.com>
2024-05-29 13:42:26 +0300

    Account category management

--- src/main/java/com/example/demo/GlobalControllerAdvice.java
+++ src/main/java/com/example/demo/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.exception.AccountCategoryNotFoundException;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.pojo.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,14 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException exception) {
+        ErrorResponse wsResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND.value())
+                .body(wsResponse);
+    }
+
+    @ExceptionHandler(AccountCategoryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleAccountCategoryNotFoundException(AccountCategoryNotFoundException exception) {
         ErrorResponse wsResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND.value())
